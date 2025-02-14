bbedff2aafbf96136d03dbb7cfd2e876fef23aab
Abe Mugwe <abraham.mugwe@strathmore.edu>
2025-02-14 14:02:16 +0300

    Abe connected database again

---
+++ UCM/src/UCM_DB_Connector.java
@@ -0,0 +1,27 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class UCM_DB_Connector {
+    private final String url = "jdbc:postgresql://localhost:5432/ucm";
+    private final String user = "postgres";
+    private final String password = "Crossword25";
+
+    public void connect() {
+        try (Connection conn = DriverManager.getConnection(url, user, password);) {
+            if (conn != null) {
+                System.out.println("Connected to Mamas!");
+            } else {
+                System.out.println("Failed to connect to Mamas");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+    public static void main(String[] args) {
+        UCM_DB_Connector connector = new UCM_DB_Connector();
+        connector.connect();
+    }
+
+}
