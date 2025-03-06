abad0706e6ba297c9dab00c22c46196ecdd4e90b
Abe Mugwe <abraham.mugwe@strathmore.edu>
2025-03-06 14:32:50 +0300

    statistics

--- UCM/src/CreateTable.java
+++ UCM/src/CreateTable.java
@@ -8,7 +8,7 @@ public class CreateTable{
         UCM_DB_Connector stdConn = new UCM_DB_Connector();
         connect=stdConn.getConnection();
         try{
-            String query="create table student(Id VARCHAR(4) primary key,unitId VARCHAR(4),instructorName VARCHAR(100),instructorEmail VARCHAR(100) ,instructorTelephone VARCHAR(4)) ";
+            String query="create table studentFeedback(feedbackId VARCHAR(4) primary key,stdId VARCHAR(4),stdComment TEXT)";
             stmt=connect.createStatement();
             stmt.executeUpdate(query);
             System.out.println("NIGERUNDAYOO!!");
---
+++ UCM/src/Statistics.java
@@ -0,0 +1,3 @@
+public interface Statistics {
+    void average(int a,int b,int c,int d,int e,int f,int g);
+}
